cad43e0b15e844968ff2bb612b710ea34cb8ab1f
Kenneth Jones <kjones404@outlook.com>
2018-05-06 11:17:19 -0500

    Feat: create data.js

---
+++ js/data.js
@@ -0,0 +1,52 @@
+var contacts = [{
+    number: "9999999991",
+    title: "contact 1",
+    description: "contact info",
+    hours: {
+      mon: "9am - 5pm",
+      tue: "9am - 5pm",
+      wed: "9am - 5pm",
+      thur: "9am - 5pm",
+      fri: "9am - 5pm",
+      sat: "Closed",
+      sun: "Closed"
+    },
+    key: ["keywords"],
+    workgroup: ["workgroup"],
+    type: "external"
+  },
+  {
+    number: "9999999992",
+    title: "contact 2",
+    description: "contact info",
+    hours: {
+      mon: "9am - 5pm",
+      tue: "9am - 5pm",
+      wed: "9am - 5pm",
+      thur: "9am - 5pm",
+      fri: "9am - 5pm",
+      sat: "Closed",
+      sun: "Closed"
+    },
+    key: ["keywords"],
+    workgroup: ["workgroup"],
+    type: "external"
+  },
+  {
+    number: "9999999993",
+    title: "contact 3",
+    description: "contact info",
+    hours: {
+      mon: "9am - 5pm",
+      tue: "9am - 5pm",
+      wed: "9am - 5pm",
+      thur: "9am - 5pm",
+      fri: "9am - 5pm",
+      sat: "Closed",
+      sun: "Closed"
+    },
+    key: ["keywords"],
+    workgroup: ["workgroup"],
+    type: "internal"
+  }
+];
